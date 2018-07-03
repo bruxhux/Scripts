9fdb6634bb599f34e619e693f1cca45536b1ee65
bruxhux <40693814+bruxhux@users.noreply.github.com>
2018-07-03 17:08:26 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,11 +2,6 @@
 
 yum -y update
 yum -y install epel-release
-wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
 yum -y install nginx
 systemctl start nginx
-firewall-cmd --permanent --zone=public --add-service=http 
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --reload
 systemctl enable nginx
